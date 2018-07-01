f9d14e5fd6e8925e1e484f667eaaef998c06a125
PiTrem <pierre.tremouilhac@gmx.de>
2018-07-01 14:35:57 +0200

    stub current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
+User = Struct.new(:id, :is_templates_moderator)
+
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def set_current_user
+    @current_user ||= User.new(0, true)
+  end
+
+  def current_user
+    @current_user
+  end
 end
+
